a8edec6492e2b8589cdd16632a7b52a99940ac5a
MolchanovAlexander <rybkolhoz@gmail.com>
2024-08-30 09:55:42 +0300

    not ok

--- src/main/java/com/example/first_camel_case/FirstCamelCaseApplication.java
+++ src/main/java/com/example/first_camel_case/FirstCamelCaseApplication.java
@@ -2,8 +2,10 @@ package com.example.first_camel_case;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:camel/camel-context.xml")
 public class FirstCamelCaseApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/first_camel_case/route/MyFirstRouteBuilder.java
+++ src/main/java/com/example/first_camel_case/route/MyFirstRouteBuilder.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component
 public class MyFirstRouteBuilder extends RouteBuilder {
 
     @Value("${app.queue.consume}")
