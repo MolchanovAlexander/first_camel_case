2df122c589cf43b11a1ed21afb9513c9ebd7e5be
MolchanovAlexander <rybkolhoz@gmail.com>
2024-12-15 17:12:24 +0200

    started work

--- src/main/java/com/example/first_camel_case/FirstCamelCaseApplication.java
+++ src/main/java/com/example/first_camel_case/FirstCamelCaseApplication.java
@@ -2,12 +2,15 @@ package com.example.first_camel_case;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.JmsAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ImportResource(locations = {
-		"classpath:camel/camel-context.xml",
-		"classpath:camel/camel-context2.xml"
+@ImportResource(locations = { "classpath:camel/two_brokers_context.xml"
+//		"classpath:camel/camel-context.xml",
+//		"classpath:camel/camel-context2.xml"
 })
 public class FirstCamelCaseApplication {
 
